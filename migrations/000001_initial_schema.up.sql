6329b6d0c06b7212b8793fbba9a54e44dc96457d
Andrii Kolomiichenko <bboywilld@gmail.com>
2025-08-09 16:16:56 +0200

    Rework migrations

---
+++ migrations/000001_initial_schema.up.sql
@@ -0,0 +1,9 @@
+-- Legacy migration - already applied via initdb
+-- Original file: 001_initial_schema.sql
+
+BEGIN;
+
+-- This migration was already applied during database initialization
+-- Content will be added here for reference after transition
+
+COMMIT;
---
+++ migrations/000004_uk_localizations.up.sql
@@ -0,0 +1,9 @@
+-- Legacy migration - already applied via initdb
+-- Original file: 004_uk_localizations.sql
+
+BEGIN;
+
+-- This migration was already applied during database initialization
+-- Content will be added here for reference after transition
+
+COMMIT;
