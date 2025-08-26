0d34ae3c8e98a2fdc766c42d7cf99d7713e1f95a
dmitry <dm.selezen@ukr.net>
2025-08-26 16:25:42 +0300

    add rollback

--- migrations/000012_localizations.down.sql
+++ migrations/000012_localizations.down.sql
@@ -1,1 +1,3 @@
--- No rollback needed
+DELETE FROM localizations WHERE key ='wrongcapcha_mes';
+DELETE FROM localizations WHERE key ='banactive_mes';
+DELETE FROM localizations WHERE key ='banactive_mes3';
